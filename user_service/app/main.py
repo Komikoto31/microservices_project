3ce874a5628dff44407d771d07d13c6e2e4a827f
Komikoto <danyashihov98@gmail.com>
2025-11-16 17:35:40 +0300

    Fix service account roles

--- user_service/app/main.py
+++ user_service/app/main.py
@@ -4,3 +4,4 @@ from app.endpoints import user_router
 app = FastAPI(title='User Service', description='Service for user management and authentication')
 
 app.include_router(user_router, prefix='/api')"# Test comment for CI/CD" 
+" " 
